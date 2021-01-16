22118d244a5f6ee6b2dd318ff416e6cbbe61cea7
Nosmoth <fedioun.achille@gmail.com>
2021-01-16 16:44:14 +0100

    Add loading csv$

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import nibabel as nib
 import time, os
 import matplotlib.pyplot as plt
 import numpy as np
+import csv
 
 def main():
 
@@ -12,10 +13,15 @@ def main():
 	dataset_folder = os.path.join(input_folder, data_type)
 	patients = os.listdir(dataset_folder)
 
-	
+
+	csv_path = os.path.join(input_folder, "survival_data.csv")
+
+
+	patients_dict = get_metadata(os.path.join(input_folder, "survival_data.csv"))
 
 	for p in patients:
 		load_patient(os.path.join(dataset_folder, p))
+
 
 	exit()
 
@@ -29,16 +35,10 @@ def main():
 
 
 def load_patient(path):
-
 	y = nib.load(os.path.join(path, os.path.basename(path) + "_seg.nii.gz")).get_fdata()
-
-
 	channels = ["t1", "t1ce", "t2", "flair"]
-
 	tmp = []
-
 	for c in channels:
-
 		tmp.append(
 			nib.load(os.path.join(path, os.path.basename(path) + "_" + c + ".nii.gz")).get_fdata()
 		)
@@ -47,8 +47,23 @@ def load_patient(path):
 
 	return x, y
 
-def get_metadata(id, csv_path):
-	pass
+def get_metadata(csv_path):
+
+	csv_path = os.path.join(input_folder, "survival_data.csv")
+	patients_dict = {}
+
+	with open(csv_path) as csv_file:
+		lines = [line for line in csv.reader(csv_file, delimiter=',', quotechar='|')][1:]
+
+		for line in lines:
+			dt = {
+				'Age' : line[1],
+				'Survival' : line[2],
+				'ResectionStatus' : line[3]
+			}
+			patients_dict[line[0]] = dt
+
+	return patients_dict
 
 
 
